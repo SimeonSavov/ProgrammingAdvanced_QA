5c95b4c3f2eba0855501a34da8460d68a5637370
Simeon <simeonsavov21@gmail.com>
2023-11-07 08:03:02 +0200

    Add Task 2 - Repeat String Characters
    In Task 2, we are tasked with taking an array of words as input, repeating each character in each word according to its position in the word, and then displaying the resulting string. This task is a great opportunity to exercise our skills in string manipulation and looping, specifically in the context of creating a new string by repeating characters.

---
+++ 01.StringAndTextProcessing_Lab/02.RepeatStrings/RepeatStrings.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+string[] array = Console.ReadLine().Split();
+
+StringBuilder output = new();
+
+for (int i = 0; i < array.Length; i++)
+{
+    string currentWord = array[i];
+
+	for (int j = 0; j < currentWord.Length; j++)
+	{
+		output.Append(currentWord);
+	}
+}
+
+Console.WriteLine(output);
+
