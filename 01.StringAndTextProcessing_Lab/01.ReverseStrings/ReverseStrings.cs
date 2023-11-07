d629616a0f35ed78454a2a33cc0270718bdff13d
Simeon <simeonsavov21@gmail.com>
2023-11-07 08:01:44 +0200

    Add Task 1 - Reverse String
    Task 1 involves creating a program that reads strings from the console, reverses each string, and then displays the original and reversed strings. This task tests our skills in string manipulation and looping.
    
    The commit introduces Task 1, describing the problem statement and the solution's approach, which focuses on reversing strings and displaying the results.

---
+++ 01.StringAndTextProcessing_Lab/01.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,10 @@
+﻿string command = Console.ReadLine();
+
+while (command != "end")
+{
+    string reversedWord = string.Join("", command.ToCharArray().Reverse());
+
+    Console.WriteLine($"{command} = {reversedWord}");
+
+    command = Console.ReadLine();
+}
