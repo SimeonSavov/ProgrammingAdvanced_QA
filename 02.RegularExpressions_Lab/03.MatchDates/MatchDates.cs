01b5c5c152a43653132189a7c70cf45e719f4802
Simeon <simeonsavov21@gmail.com>
2023-11-08 08:23:14 +0200

    Add regular expression pattern for matching and extracting dates
     The pattern, defined as @"\b(?<Day>\d{2})(?<sep>[\.\-\/])(?<Month>[A-Z][a-z]{2})\k<sep>(?<Year>\d{4})\b", efficiently identifies and extracts dates in various valid formats in the input text using the Regex class. The commit encompasses code for user input, pattern application, and displaying the extracted date components (day, month, and year), enhancing my proficiency in using regular expressions for complex pattern extraction tasks.

---
+++ 02.RegularExpressions_Lab/03.MatchDates/MatchDates.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\b(?<Day>\d{2})(?<sep>[\.\-\/])(?<Month>[A-Z][a-z]{2})\k<sep>(?<Year>\d{4})\b";
+Regex regex = new Regex(pattern);
+
+
+string input = Console.ReadLine();
+
+MatchCollection matches = regex.Matches(input);
+
+foreach (Match match in matches)
+{
+    Console.WriteLine($"Day: {match.Groups["Day"]}, Month: {match.Groups["Month"]}, Year: {match.Groups["Year"]}");
+}
