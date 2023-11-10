fe0ab8fa2f3871296a29d8c19329858abfd1f2d4
Simeon <simeonsavov21@gmail.com>
2023-11-10 19:07:01 +0200

    Add EmailValidator and Unit Tests
    Implemented the `IsValidEmail` method in the `EmailValidator` class for validating email addresses. Utilized a regular expression pattern to check the validity of the provided email. Added corresponding unit tests in the `EmailValidatorTests` class, covering both valid and invalid email scenarios. The tests ensure the method accurately distinguishes between valid and invalid email formats.

---
+++ 03.UnitTestingStringsRegEx_Exercises/TestApp.UnitTests/EmailValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+    [TestCase("simosavov@abv.bg")]
+    [TestCase("simeon_savov@gmail.com")]
+    [TestCase("simeon+21@yahoo.com")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [TestCase("invalid mail@valid.domain")]
+    [TestCase("valid%mail@in_valid.domain")]
+    [TestCase("invalid/mail@invalid.d")]
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
