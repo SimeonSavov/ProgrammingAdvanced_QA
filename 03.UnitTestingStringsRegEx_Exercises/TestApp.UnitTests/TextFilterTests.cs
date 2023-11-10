16d40799574b4e33d72bed9470f9ebc86ac9f18f
Simeon <simeonsavov21@gmail.com>
2023-11-10 19:39:11 +0200

    Add TextFilter class and Unit Tests
    Implemented the `Filter` method in the `TextFilter` class, which replaces occurrences of banned words in the input text with asterisks. The method iterates through the list of banned words, performs case-sensitive matching, and ensures accurate replacement. Unit tests cover scenarios such as no banned words, replacing a banned word, empty list of banned words, and banned words containing whitespace. This implementation provides a robust solution for filtering out prohibited words from text.

---
+++ 03.UnitTestingStringsRegEx_Exercises/TestApp.UnitTests/TextFilterTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class TextFilterTests
+{
+    [Test]
+    public void Test_Filter_WhenNoBannedWords_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string[] bannedWords = new[] { "cat" };
+        string text = "The quick brown fox jumps over the lazy dog";
+
+        // Act
+        string result = TextFilter.Filter(bannedWords, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(text));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordExists_ShouldReplaceBannedWordWithAsterisks()
+    {
+        // Arrange
+        string[] bannedWords = new[] { "quick" };
+        string text = "The quick brown fox jumps over the lazy dog";
+        string expected = "The ***** brown fox jumps over the lazy dog";
+
+        // Act
+        string result = TextFilter.Filter(bannedWords, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsAreEmpty_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string[] bannedWords = Array.Empty<string>();
+        string text = "The quick brown fox jumps over the lazy dog";
+
+        // Act
+        string result = TextFilter.Filter(bannedWords, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(text));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsContainWhitespace_ShouldReplaceBannedWord()
+    {
+        // Arrange
+        string[] bannedWords = new[] { "quick brown", "dog" };
+        string text = "The quick brown fox jumps over the lazy dog";
+        string expected = "The *********** fox jumps over the lazy ***";
+
+        // Act
+        string result = TextFilter.Filter(bannedWords, text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
