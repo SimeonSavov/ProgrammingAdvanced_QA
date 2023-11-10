59b647e3112dcb751e113bd0112d9500a6fcc031
Simeon <simeonsavov21@gmail.com>
2023-11-10 19:10:47 +0200

    Add MatchedDates method and Unit Tests
    Added the `Match` method to the `MatchDates` class, facilitating the extraction of date components (day, month, year) from a given input string. Incorporated regular expressions to identify valid date patterns. Unit tests cover various scenarios, including valid dates, no matches, multiple matches, empty string, and handling null input, ensuring the reliability of the date matching logic.

---
+++ 03.UnitTestingStringsRegEx_Exercises/TestApp.UnitTests/MatchDatesTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class MatchDatesTests
+{
+    [Test]
+    public void Test_Match_ValidDate_ReturnsExpectedResult()
+    {
+        // Arrange
+        string date = "31-Dec-2022 random text";
+        string expected = "Day: 31, Month: Dec, Year: 2022";
+
+        // Act
+        string result = MatchDates.Match(date);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_NoMatch_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "Invalid date format";
+        string expected = string.Empty;
+
+        // Act
+        string result = MatchDates.Match(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_MultipleMatches_ReturnsFirstMatch()
+    {
+        // Arrange
+        string date = "21.Jan.1999 31-Dec-2022 random text";
+        string expected = "Day: 21, Month: Jan, Year: 1999";
+
+        // Act
+        string result = MatchDates.Match(date);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_EmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = string.Empty;
+        string expected = string.Empty;
+
+        // Act
+        string result = MatchDates.Match(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_NullInput_ThrowsArgumentException()
+    {
+        // Arrange
+        string? date = null;
+
+        // Act + Assert
+        Assert.That(() => MatchDates.Match(date), Throws.ArgumentException);
+    }
+}
