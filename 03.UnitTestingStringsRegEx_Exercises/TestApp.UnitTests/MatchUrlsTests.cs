8614e536a340e51553d32ce751000152787c9c4a
Simeon <simeonsavov21@gmail.com>
2023-11-10 19:25:38 +0200

    Add MatchURLs class and Unit Tests
    Added the `ExtractUrls` method to the `MatchUrls` class, implementing logic to extract URLs from a given text. Utilized regular expressions to identify and collect valid URLs, considering variations with or without "www" and handling URLs within quotation marks. Unit tests cover scenarios with empty text, no valid URLs, a single URL, multiple URLs, and URLs within quotation marks, ensuring the accuracy of the URL extraction logic.

---
+++ 03.UnitTestingStringsRegEx_Exercises/TestApp.UnitTests/MatchUrlsTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class MatchUrlsTests
+{
+    [Test]
+    public void Test_ExtractUrls_EmptyText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "";
+        List<string> expected = new();
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_NoUrlsInText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "No valid URLs!!!";
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_ExtractUrls_SingleUrlInText_ReturnsSingleUrl()
+    {
+        // Arrange
+        string text = "Single URL: https://softuni.bg";
+        List<string> expected = new() { "https://softuni.bg" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_MultipleUrlsInText_ReturnsAllUrls()
+    {
+        // Arrange
+        string text = "Multiple URLs: https://www.softuni.bg, https://creative.softuni.bg";
+        List<string> expected = new() { "https://www.softuni.bg", "https://creative.softuni.bg" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_UrlsInQuotationMarks_ReturnsUrlsInQuotationMarks()
+    {
+        // Arrange
+        string text = "Single URL: \"https://softuni.bg/about\"";
+        List<string> expected = new() { "https://softuni.bg" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
