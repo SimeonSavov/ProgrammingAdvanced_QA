156eb765f8174440bd85efcde9f12d8a18060bfa
Simeon <simeonsavov21@gmail.com>
2023-11-14 10:02:30 +0200

    Add odd frequency word extraction
    In this task, I developed a program to extract and display words with an odd frequency from a user-provided list of words. The program utilizes a `Dictionary<string, int>` to keep track of word frequencies, ignoring case sensitivity. This exercise deepened my understanding of dictionary operations and case-insensitive comparisons.

---
+++ 04.DictionariesLambdaLINQ_Lab/02.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,26 @@
+﻿string[] words = Console.ReadLine().Split();
+
+Dictionary<string, int> wordsCount = new();
+
+foreach (string word in words)
+{
+    string caseInsensetiveWord = word.ToLower();
+
+    if (wordsCount.ContainsKey(caseInsensetiveWord))
+    {
+        wordsCount[caseInsensetiveWord]++;
+    }
+    else
+    {
+        wordsCount[caseInsensetiveWord] = 1;
+    }
+}
+
+
+foreach (KeyValuePair<string, int> pair in wordsCount)
+{
+    if (pair.Value % 2 != 0)
+    {
+        Console.Write(pair.Key + " ");
+    }
+}
