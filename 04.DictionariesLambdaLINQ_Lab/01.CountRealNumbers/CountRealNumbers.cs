bc7802aee166df0bc9b9b139d44dba7d792b3948
Simeon <simeonsavov21@gmail.com>
2023-11-14 10:00:40 +0200

    Add frequency analysis of numbers
    In this exercise, I implemented a program to analyze the frequency of numbers entered by the user. The program reads a sequence of integers, counts the occurrences of each number, and displays the results using a `SortedDictionary<int, int>`. This exercise enhanced my understanding of dictionary usage and frequency analysis.

---
+++ 04.DictionariesLambdaLINQ_Lab/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+SortedDictionary<int, int> numbersFrequency = new();
+
+foreach (int num in numbers)
+{
+    if (numbersFrequency.ContainsKey(num))
+    {
+        numbersFrequency[num]++;
+    }
+    else
+    {
+        numbersFrequency[num] = 1;
+    }
+}
+
+foreach (KeyValuePair<int, int> pair in numbersFrequency)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
