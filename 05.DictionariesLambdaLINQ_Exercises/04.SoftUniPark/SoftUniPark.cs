26da0c9669500ac57a1eabda060ee73ba40fb008
Simeon <simeonsavov21@gmail.com>
2023-11-15 15:48:08 +0200

    Add employee registration
    Implemented a program for registering and unregistering employees along with their plate numbers. The program utilizes a Dictionary<string, string> to store and manage employee registrations. This exercise enhanced my skills in working with dictionaries, handling user commands, and providing meaningful feedback.

---
+++ 05.DictionariesLambdaLINQ_Exercises/04.SoftUniPark/SoftUniPark.cs
@@ -0,0 +1,43 @@
+﻿Dictionary<string, string> registered = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] commandArray = Console.ReadLine().Split().ToArray();
+
+    string command = commandArray[0];
+    string employee = commandArray[1];
+
+    if (command == "register")
+    { 
+        string plateNumber = commandArray[2];
+
+        if (!registered.ContainsKey(employee))
+        {
+            registered.Add(employee, plateNumber);
+            Console.WriteLine($"{employee} registered {plateNumber} successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: already registered with plate number {registered[employee]}");
+        }
+    }
+    else if (command == "unregister")
+    {
+        if (registered.ContainsKey(employee))
+        {
+            registered.Remove(employee);
+            Console.WriteLine($"{employee} unregistered successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: user {employee} not found");
+        }
+    }
+}
+
+foreach (var pair in registered)
+{
+    Console.WriteLine($"{pair.Key} => {pair.Value}");
+}
