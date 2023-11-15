fc1c43109f6f59c41cccf3edb8243cee36431bec
Simeon <simeonsavov21@gmail.com>
2023-11-15 16:43:56 +0200

    Add track company users
    Implemented a program to track employees assigned to different companies. The program uses a Dictionary<string, List<string>> to store company names and the corresponding employee IDs. This exercise enhanced my skills in working with dictionaries and managing relationships between data.

---
+++ 05.DictionariesLambdaLINQ_Exercises/06.CompUsers/CompanyUsers.cs
@@ -0,0 +1,32 @@
+﻿Dictionary<string, List<string>> companies = new();
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] inputArray = input.Split(" -> ");
+    string company = inputArray[0];
+    string employeeId = inputArray[1];
+
+    if (!companies.ContainsKey(company))
+    {
+        companies.Add(company, new List<string>());
+    }
+
+    if (!companies[company].Contains(employeeId))
+    {
+        companies[company].Add(employeeId);
+    }
+    
+    input = Console.ReadLine();
+}
+
+foreach (var currentCompany in companies)
+{
+    Console.WriteLine(currentCompany.Key);
+
+    foreach (string currentEmployeeId in currentCompany.Value)
+    {
+        Console.WriteLine($"-- {currentEmployeeId}");
+    }
+}
