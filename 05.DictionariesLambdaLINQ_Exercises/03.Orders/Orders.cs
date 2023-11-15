e061f9cf0d435bf547be8de98c603723e66fef27
Simeon <simeonsavov21@gmail.com>
2023-11-15 11:00:39 +0200

    Add product tracker
    Implemented a program for tracking product prices and quantities, providing a total cost for each product. The program utilizes a Dictionary<string, List<decimal>> to store and update product information based on user input. This exercise furthered my understanding of working with dictionaries and managing complex data structures.

---
+++ 05.DictionariesLambdaLINQ_Exercises/03.Orders/Orders.cs
@@ -0,0 +1,34 @@
+﻿Dictionary<string, List<decimal>> products = new();
+
+string input = Console.ReadLine();
+
+while (input != "buy")
+{
+    string[] inputArray = input.Split();
+    string product = inputArray[0];
+    decimal price = decimal.Parse(inputArray[1]);
+    decimal quantity = decimal.Parse(inputArray[2]);
+
+    if (!products.ContainsKey(product))
+    {
+        products.Add(product, new List<decimal>() { price, quantity });
+    }
+    else
+    {
+        products[product][0] = price;
+        products[product][1] += quantity;
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (var pair in products)
+{
+    string currentProductName = pair.Key;
+    decimal currentProductPrice = pair.Value[0];
+    decimal currentProductQuantity = pair.Value[1];
+
+    decimal currentProductAmount = currentProductPrice * currentProductQuantity;
+
+    Console.WriteLine($"{currentProductName} -> {currentProductAmount:f2}");
+}
