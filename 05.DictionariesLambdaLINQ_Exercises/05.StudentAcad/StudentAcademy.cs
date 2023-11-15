e478cf0cb2ace36c02d600ce0b838d5ac94d6801
Simeon <simeonsavov21@gmail.com>
2023-11-15 16:00:16 +0200

    Add excellent students
    Implemented a program to identify excellent students based on their average grades. The program uses a Dictionary<string, List<double>> to store student names and their corresponding grades. This exercise reinforced my understanding of dictionaries, LINQ expressions, and filtering data based on specified conditions.

---
+++ 05.DictionariesLambdaLINQ_Exercises/05.StudentAcad/StudentAcademy.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, List<double>> students = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string studentName = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(studentName))
+    {
+        students.Add(studentName, new List<double>());
+    }
+
+    students[studentName].Add(grade);   
+}
+
+var excellent = students.Where(s => s.Value.Average() >= 4.50); // 's' -> students / This is dictionary
+
+foreach (var currentStudent in excellent)
+{
+    Console.WriteLine($"{currentStudent.Key} -> {currentStudent.Value.Average():f2}");
+}
