f6a01e754c975a0681af63ad1b4268f447bf3dbe
Simeon <simeonsavov21@gmail.com>
2023-11-17 20:13:59 +0200

    Add unit tests for Plants class
    Implemented NUnit tests for the Plants class, covering scenarios such as an empty array, a single plant, and multiple plants. The tests ensure that the GetFastestGrowing method correctly groups and orders plants by their lengths. All tests pass successfully, confirming the functionality of the Plants class.

---
+++ 06.UnitTesting_Dictionaries_Exercises/TestApp.Tests/PlantsTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Numerics;
+
+namespace TestApp.Tests;
+
+public class PlantsTests
+{
+    [Test]
+    public void Test_GetFastestGrowing_WithEmptyArray_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] plants = new string[0];
+
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetFastestGrowing_WithSinglePlant_ShouldReturnPlant()
+    {
+        // Arrange
+        string[] plants = new string[] 
+        { 
+            "roses" // 5 symbols here <-
+        };
+
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 5 letters:\r\nroses"));
+    }
+
+    [Test]
+    public void Test_GetFastestGrowing_WithMultiplePlants_ShouldReturnGroupedPlants()
+    {
+        // Arrange
+        string[] plants = new string[]
+        {
+            "roses",
+            "lily",
+            "maple",
+            "mango"
+        };
+
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 4 letters:\r\nlily\r\n" +
+            "Plants with 5 letters:\r\nroses\r\nmaple\r\nmango"));
+    }
+
+    [Test]
+    public void Test_GetFastestGrowing_WithMixedCasePlants_ShouldBeCaseInsensitive()
+    {
+        // Arrange
+        string[] plants = new string[]
+        {
+            "Rose",
+            "lIly",
+            "aBcde",
+            "fdGre"
+        };
+
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 4 letters:\r\nRose\r\nlIly\r\n" +
+            "Plants with 5 letters:\r\naBcde\r\nfdGre"));
+    }
+}
