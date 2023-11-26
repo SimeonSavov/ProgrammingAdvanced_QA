3c3d0c16fcf1b81b27ddc898db8b0b564150f3b1
Simeon <simeonsavov21@gmail.com>
2023-11-26 18:46:59 +0200

    Add source code for the Students program
    Added the source code for the Students program in C#. The program defines a Student class with properties for FirstName, LastName, and Grade. It also includes a Students class with a Main method that reads student information, creates Student objects, and outputs the students' details sorted by grade in descending order. The program uses a List to manage the collection of students.

---
+++ 09.ObjectAndClasses_Exercises/01.Students/Students.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _01.Students
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+    }
+
+
+    public class Students
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                string firstName = command[0];
+                string lastName = command[1];
+                double grade = double.Parse(command[2]);
+
+                Student currentStudent = new Student
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Grade = grade
+                };
+
+                students.Add(currentStudent);
+            }
+
+            students = students.OrderByDescending(s => s.Grade).ToList();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
+        }
+    }
+}
