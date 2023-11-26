146780efc6e542aa9519b1a652013b45381ff507
Simeon <simeonsavov21@gmail.com>
2023-11-26 18:49:21 +0200

    Add source code for the Articles program
    Included the source code for the Articles program in C#. The program defines an Article class with properties for Title, Content, and Author, along with methods to edit content, change the author, and rename the article. The Articles class contains a Main method that reads initial article information, performs specified actions based on user input, and outputs the modified article details. The program utilizes the switch statement to handle different actions like editing, changing the author, and renaming the article.

---
+++ 09.ObjectAndClasses_Exercises/02.Articles/Articles.cs
@@ -0,0 +1,75 @@
+﻿using System.Data;
+
+namespace _02.Articles
+{
+
+    public class Article
+    {
+        public Article(string newTitle, string newContent, string newAuthor) 
+        {
+           this.Content = newContent;
+           this.Author = newAuthor;
+           this.Title = newTitle;
+        }
+
+        public void Edit(string newContent)
+        {
+            this.Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            this.Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            this.Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+    public class Articles
+    {
+        static void Main(string[] args)
+        {
+            string[] articleInfo = Console.ReadLine().Split(", ");
+            string title = articleInfo[0];
+            string content = articleInfo[1];
+            string author = articleInfo[2];
+
+            Article article = new(title, content, author);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ");
+                string action = command[0];
+                string parameter = command[1];
+
+                switch (action)
+                {
+                    case "Edit":
+                        article.Edit(parameter);
+                        break;
+                    case "ChangeAuthor":
+                        article.ChangeAuthor(parameter);
+                        break;
+                    case "Rename":
+                        article.Rename(parameter);
+                        break;
+                }
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+}
