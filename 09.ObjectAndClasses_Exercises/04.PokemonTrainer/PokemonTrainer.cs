5118bedb72927acbe98921b9aac355109f366e5d
Simeon <simeonsavov21@gmail.com>
2023-11-27 16:01:49 +0200

    Add source code for the Pokemon Trainer Tournament
    Included the source code for a Pokemon Trainer Tournament program in C#. The program allows the registration of trainers and their respective Pokemon, tracks their badges, and simulates battles based on Pokemon elements. It uses a list of trainers (Trainer class) and Pokemon (Pokemon class) to manage the tournament. The first part of the program registers trainers and their Pokemon, ensuring trainers are unique and associating Pokemon with their respective trainers. The second part simulates battles by checking if a trainer has a Pokemon with a specific element and awards badges accordingly. If a trainer doesn't have a Pokemon with the required element, their Pokemon lose health. The program then removes Pokemon with zero or less health. Finally, it prints the trainers sorted by the number of badges, descending, along with the count of remaining Pokemon for each trainer.

---
+++ 09.ObjectAndClasses_Exercises/04.PokemonTrainer/PokemonTrainer.cs
@@ -0,0 +1,73 @@
+﻿string command = Console.ReadLine();
+
+List<Trainer> trainers = new List<Trainer>();
+
+while  (command != "Tournament")
+{
+    //{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}
+    string[] data = command.Split().ToArray();
+
+    Pokemon currentPokemon = new Pokemon(data[1], data[2], int.Parse(data[3]));
+
+    bool trainerExists = false;
+
+    Trainer existingTrainers = new Trainer();
+    foreach (Trainer trainer in trainers)
+    {
+        if (data[0] == trainer.Name)
+        {
+            trainerExists = true;
+            existingTrainers = trainer;
+        }
+    }
+
+    if (trainerExists)
+    {
+        existingTrainers.Pokemons.Add(currentPokemon);
+    }
+    else
+    {
+        Trainer currentTrainer = new Trainer(data[0]);
+        currentTrainer.Pokemons.Add(currentPokemon);
+        trainers.Add(currentTrainer);
+    }
+
+    command = Console.ReadLine();
+}
+
+string commandTwo = Console.ReadLine();
+
+while (commandTwo != "End")
+{
+    for (int i = 0; i < trainers.Count; i++)
+    {
+        bool hasPokemon = false;
+
+        foreach (Pokemon pokemon in trainers[i].Pokemons)
+        {
+            if (commandTwo == pokemon.Element)
+            {
+                hasPokemon = true;
+            }
+        }
+
+        if (hasPokemon)
+        {
+            trainers[i].Badges++;
+        }
+        else
+        {
+            trainers[i].Pokemons.ForEach(p => { p.Health -= 10; });
+        }
+
+        trainers[i].Pokemons.RemoveAll(x => x.Health <= 0);
+    }
+
+    commandTwo = Console.ReadLine();
+}
+
+List<Trainer> sortedTrainers = trainers.OrderByDescending(t => t.Badges).ToList();
+foreach (Trainer t in sortedTrainers)
+{
+    Console.WriteLine($"{t.Name} {t.Badges} {t.Pokemons.Count}");
+}
