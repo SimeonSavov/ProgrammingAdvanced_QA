56973960fcb8ead43114452d7b28f42f8620b9e2
Simeon <simeonsavov21@gmail.com>
2023-11-27 15:58:31 +0200

    Add source code for the Team Management program
    Included the source code for a Team Management program in C#. The program allows the creation of teams and assignment of members to teams while handling various constraints. It uses a dictionary to store teams, and each team is represented by the Team class, including properties like the team's creator, name, and a list of members. The program ensures unique team names and prevents team creators from creating multiple teams. It also checks if a member is already on a team or is the creator of the team before adding them. Finally, it prints the teams with their members, sorted by the number of members and team names, and lists teams without members.

---
+++ 09.ObjectAndClasses_Exercises/03.TeamWorkProject/Team.cs
@@ -0,0 +1,13 @@
+﻿public class Team
+{
+    public string Name { get; set; }
+    public string Creator { get; set; }
+    public List<string> Members { get; set; }
+
+    public Team(string name, string creator)
+    {
+        this.Name = name;
+        this.Creator = creator;
+        this.Members = new List<string>();
+    }
+}
---
+++ 09.ObjectAndClasses_Exercises/03.TeamWorkProject/TeamWorkProject.cs
@@ -0,0 +1,78 @@
+﻿int countTeams = int.Parse(Console.ReadLine());
+
+Dictionary<string, Team> teams = new Dictionary<string, Team>();
+
+for (int i = 1; i <= countTeams; i++)
+{
+    string[] teamData = Console.ReadLine().Split("-").ToArray();
+    string creator = teamData[0];
+    string teamName = teamData[1];
+
+    if (teams.ContainsKey(teamName))
+    {
+        Console.WriteLine($"Team {teamName} was already created!");
+        continue;
+    }
+
+    if (teams.Any(t => t.Value.Creator == creator))
+    {
+        Console.WriteLine($"{creator} cannot create another team!");
+        continue;
+    }
+
+    Team team = new Team(teamName, creator);
+    Console.WriteLine($"Team {teamName} has been created by {creator}!");
+
+    teams.Add(teamName, team);
+}
+
+string command = Console.ReadLine();
+
+while (command != "end of assignment")
+{
+    string memberJoin = command.Split("->")[0];
+    string teamJoining = command.Split("->")[1];
+
+    if (!teams.ContainsKey(teamJoining))
+    {
+        Console.WriteLine($"Team {teamJoining} does not exist!");
+    }
+    else
+    {
+        bool memberAlreadyOnTeam = teams.Values.Any(t => t.Members.Contains(memberJoin));
+
+        if (memberAlreadyOnTeam || memberJoin == teams[teamJoining].Creator)
+        {
+            Console.WriteLine($"Member {memberJoin} cannot join team {teamJoining}!");
+        }
+        else
+        {
+            teams[teamJoining].Members.Add(memberJoin); // Add "member" to "team"
+        }
+    }
+
+    command = Console.ReadLine();
+}
+
+// Sorted and print the team with members only!
+foreach (var team in teams.Where(team => team.Value.Members.Count > 0)
+                          .OrderByDescending(team => team.Value.Members.Count) 
+                          .ThenBy(team => team.Key))
+{
+    // Sorted Dictionary with teams with members in there!
+    Console.WriteLine($"{team.Key}");
+    Console.WriteLine($"- {team.Value.Creator}");
+
+    foreach (string member in team.Value.Members.OrderBy(m => m))
+    {
+        Console.WriteLine($"-- {member}");
+    }
+}
+
+// Print teams without members
+Console.WriteLine($"Teams to disband: ");
+foreach (var team in teams.Where(team => team.Value.Members.Count == 0)
+                          .OrderBy(team => team.Key)) // Teams without members!
+{
+    Console.WriteLine($"{team.Key}");
+}
