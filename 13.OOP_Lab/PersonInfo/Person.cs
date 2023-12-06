8cb446c4a9c1b49fb09a9852411ad546970c9621
Simeon <simeonsavov21@gmail.com>
2023-12-06 16:47:18 +0200

    Add Person Class and Sorting Logic
    In this commit, I introduced the Person class encapsulating properties for first name, last name, and age, enforcing constraints for minimum lengths and positive age values. The class includes a ToString method for easy string representation. Additionally, a console application (PersonInfo) was created to input a list of persons, instantiate the Person class, and display sorted results based on first name and age using LINQ's OrderBy and ThenBy methods. Exception handling ensures that valid person information is provided.

---
+++ 13.OOP_Lab/PersonInfo/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PersonInfo;
+
+public class Person
+{
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    public string FirstName
+    {
+        get { return _firstName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols");
+            }
+
+            _firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return _lastName; }
+        private set 
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols");
+            }
+            _lastName =  value; 
+        }
+    }
+
+    public int Age
+    {
+        get { return _age; }
+        private set 
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer");
+            }
+
+            _age = value; 
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+    }
+}
