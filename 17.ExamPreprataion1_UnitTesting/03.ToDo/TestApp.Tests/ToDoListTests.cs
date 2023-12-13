7e0519984f44a65b1635f24213837522482fc7ed
Simeon <simeonsavov21@gmail.com>
2023-12-13 10:06:54 +0200

    Add ToDoList class for Task Management and Unit Tests
    Implemented the ToDoList class, providing functionality for adding tasks, marking tasks as completed, and displaying the to-do list. The class utilizes a list of TaskItem objects, and the methods are designed to handle various scenarios such as completing tasks, task not found, and displaying tasks. Unit tests in ToDoListTests validate the correctness of the implemented methods, covering scenarios like adding tasks, completing tasks, attempting to complete a non-existent task, and displaying tasks. This class serves as a practical utility for managing and tracking tasks in a to-do list.

---
+++ 17.ExamPreprataion1_UnitTesting/03.ToDo/TestApp.Tests/ToDoListTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using NUnit.Framework;
+
+using TestApp.Todo;
+
+namespace TestApp.Tests;
+
+[TestFixture]
+public class ToDoListTests
+{
+    private ToDoList _toDoList = null!;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        this._toDoList = new();
+    }
+    
+    [Test]
+    public void Test_AddTask_TaskAddedToToDoList()
+    {
+        // Arrange
+        string title = "My first task";
+        DateTime time = DateTime.Today;
+
+        this._toDoList.AddTask(title, time);
+
+        // Act
+        var result = this._toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Does.Contain($"[ ] My first task - Due:"));
+    }
+
+    [Test]
+    public void Test_CompleteTask_TaskMarkedAsCompleted()
+    {
+        // Arrange
+        string title = "To complete";
+        DateTime time = DateTime.Today;
+
+        this._toDoList.AddTask(title, time);
+        this._toDoList.CompleteTask(title);
+
+        // Act
+        var result = this._toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Does.Contain($"[✓] To complete - Due:"));
+    }
+
+    [Test]
+    public void Test_CompleteTask_TaskNotFound_ThrowsArgumentException()
+    {
+        // Arrange
+        string title = "To complete this one";
+
+        // Act + Assert
+        Assert.That(() => this._toDoList.CompleteTask(title), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_DisplayTasks_NoTasks_ReturnsEmptyString()
+    {
+        // Arrange
+
+        // Act
+        string result = this._toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Is.EqualTo("To-Do List:"));
+    }
+
+    [Test]
+    public void Test_DisplayTasks_WithTasks_ReturnsFormattedToDoList()
+    {
+        // Arrange
+        string title = "My first task";
+        DateTime time = DateTime.Today;
+        this._toDoList.AddTask(title, time);
+
+        string title2 = "To complete";
+        DateTime time2 = DateTime.Today;
+        this._toDoList.AddTask(title2, time2);
+        this._toDoList.CompleteTask(title2);
+
+        // Act
+        var result = this._toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Does.Contain($"[ ] My first task - Due:"));
+        Assert.That(result, Does.Contain($"[✓] To complete - Due:"));
+    }
+}
